59ea290808d56229a3933bfcf5854c501957d1a4
Magne Alver Normann <magnealv@dmlab06.(none)>
2015-02-27 17:15:38 +0100

    added enum

--- ex2/songs.h
+++ ex2/songs.h
@@ -2,46 +2,66 @@
 #define SONGS_H
 
 
-//TUNES
+typedef enum {
+    WIN,
+    LOSE,
+    HIT,
+    MISS,
+    IMPERIAL,
+    N_TUNES
+} Tune_nr;
 
+typedef struct tune{
+    Note* melody;
+    uint32_t length;
+    uint32_t current_note;
+    uint32_t use_fadeout;
+}tune_t
+
+
+
+/* TONES USED */
 volatile Note g500ms = { .note = 'g', .octave = 4, .time_to_play_ms = 500 };
 volatile Note e500ms = { .note = 'e', .octave = 4, .time_to_play_ms = 300 };
 volatile Note b200ms = { .note = 'b', .octave = 4, .time_to_play_ms = 200 };
-volatile Note g1000ms = { .note = 'b', .octave = 5, .time_to_play_ms = 1000 };
+volatile Note g400ms = { .note = 'b', .octave = 5, .time_to_play_ms = 400 };
 
 volatile Note g400ms = { .note = 'g', .octave = 4, .time_to_play_ms = 400 };
 volatile Note e400ms = { .note = 'e', .octave = 4, .time_to_play_ms = 400 };
 volatile Note c800ms = { .note = 'a', .octave = 3, .time_to_play_ms = 800 };
 
-//SONGS
+/* SONGS */
 
-//Imperial march
+// Imperial march 
 volatile Note* imperial_march[9] = {&g500ms, &g500ms, &g500ms, &e500ms, &b200ms, &g500ms, &e500ms, &b200ms, &g500ms};
 volatile uint32_t imperial_march_length = 9;
 volatile uint32_t imperial_march_current_note = 0;
 volatile uint8_t use_imperial_march_fadeout = 0;
-//SOUNDS
 
-//Shoot
-volatile Note* shoot[1] = {&g1000ms};
+
+/* SOUNDS */
+
+// Shoot
+volatile Note* shoot[1] = {&g400ms};
 volatile uint32_t shoot_length = 1;
 volatile uint32_t shoot_current_note = 0;
 volatile uint8_t use_shoot_fadeout = 0;
 
-//Wallhit
+// Lose
 volatile Note* wallhit[3] = {&g400ms, &e400ms, &c800ms};
 volatile uint32_t wallhit_length = 3;
 volatile uint32_t wallhit_current_note = 0;
 volatile uint8_t use_wallhit_fadeout = 1;
 
-//OPTIONS TODO::SET TO UINT8 WHERE POSSIBLE
+
+//OPTIONS 
 volatile uint8_t song_to_use = 1;
 volatile uint8_t use_fadeout = 0;
-volatile uint32_t fade_direction = 1;
-volatile uint32_t fade_inc = 10;
+volatile uint8_t fade_direction = 1;
+volatile uint8_t fade_inc = 10;
 volatile uint32_t soundMAX = 200;
-volatile uint32_t sound0 = 80;
-volatile uint32_t sound1 = 0;
+volatile uint16_t sound0 = 80;
+volatile uint16_t sound1 = 0;
 
 void set_fadeout(uint8_t condition){
     if(condition){
